ba0e733a712e334585352ae6eabb53c5511367d3
HeKaiYu <hekaiyu_301@163.com>
2022-05-19 11:39:11 +0800

    chore: init

---
+++ src/components/Ref.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const objRef = ref({
+  a: 1,
+  b: 2
+})
+
+const changeObjRef = () => {
+  objRef.value = {
+    a: 1,
+    b: 2
+  }
+}
+
+</script>
+
+<template>
+  <p>{{ objRef }}</p>
+  <button @click="changeObjRef">改变 ObjRef</button>
+</template>
+
+<style>
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import TheWelcome from '@/components/TheWelcome.vue'
+import Ref from '@/components/Ref.vue'
+</script>
+
+<template>
+  <main>
+    <Ref />
+    <TheWelcome />
+  </main>
+</template>
